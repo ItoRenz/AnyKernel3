737f78834f3ba920b7eeaf8e4be9fd1bf238af45
ItoRenz <ridwan.ri214@gmail.com>
2021-08-28 18:42:36 +0000

    update AnyKernel3
    Signed-off-by: ItoRenz <ridwan.ri214@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -12,7 +12,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=rolex
 device.name2=redmi4a
-supported.versions=
+supported.versions=10
 supported.patchlevels=
 '; } # end properties
 
@@ -26,11 +26,6 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-ui_print "Welcome To Jura Tempest Federation"
-ui_print "               Shuna              "
-ui_print "           Oni Princess           "
-ui_print "          Tempest Family          "
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
@@ -39,19 +34,6 @@ set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 ## AnyKernel boot install
 dump_boot;
-
-ui_print "*********************"
-ui_print " "
-ui_print "SSSSSS S    S S    S SSSSSS SSSSSS"
-ui_print "S      S    S S    S S    S S    S"
-ui_print "S      S    S S    S S    S S    S"
-ui_print "SSSSSS SSSSSS S    S S    S SSSSSS"
-ui_print "     S S    S S    S S    S S    S"
-ui_print "     S S    S S    S S    S S    S"
-ui_print "SSSSSS S    S SSSSSS S    S S    S"
-ui_print "             TEMPEST              "
-ui_print "************ ItoRenz *************"
-ui_print "*******************"
 
 write_boot;
 ## end boot install
