75a887e14138d31558c28ef65445d1886f0aaa2f
ItoRenz <ridwan.ri214@gmail.com>
2021-09-28 23:15:37 +0000

    update AnyKernel3
    Signed-off-by: ItoRenz <ridwan.ri214@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,23 +4,27 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=mi8937
+device.name2=santoni
+device.name3=rolex
+device.name4=riva
+device.name5=rova
+device.name6=land
+device.name7=prada
+device.name8=ugglite
+device.name9=ugg
+device.name10=tiare
 supported.versions=
 supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
